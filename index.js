cfd0364ee3618875d4b675b10d8456a602f7d0e3
Alan Akra <alanakra@hotmail.fr>
2022-10-15 20:20:52 +0200

    Fix: init inputPwd during submit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const pwdLength = document.getElementById('inputPwd').value
 const form = document.getElementsByTagName('form')[0]
 const result = document.getElementById('result')
 const copyPwd = document.getElementById('copyPwd')
 const inputResult = document.getElementById('inputResult')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    // result.innerHTML = `Your password is ${generatePassword()}`
-    inputResult.value = generatePassword(pwdLength)
+  e.preventDefault()
+  const resultPassword = generatePassword()
+  result.innerHTML = `Your password is ${resultPassword}`
+  inputPwd.value = ''
 })
 
-function generatePassword (pwdLength) {
+function generatePassword () {
+    const inputPwd = document.getElementById('inputPwd').value
     const bigString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]{}()*;/,_-"
     const specialString = "@#$%"
 
     let pwd = ""
-
-    for (let i = 1; i < pwdLength; i++){
+    for (let i = 1; i < inputPwd; i++){
       pwd += bigString.charAt(Math.random() * bigString.length)
     }
 
